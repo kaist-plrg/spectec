7cef6b76bbbddf603cefed2d1e38421ce1ed6c38
Wonho <new170527@kaist.ac.kr>
2024-02-02 16:40:19 +0900

    Update west script

--- spectec/west.sh
+++ spectec/west.sh
@@ -1,15 +1,34 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+function run {
+  printf " - $1: ${ORANGE}Running..${NC}\r"
+  printf "${RED}"
+  $2 $3
+  res=$?
+  printf "${NC}"
+  if [[ $res = 0 ]]; then
+    printf " - $1: ${GREEN}Success${NC}  \n"
+  fi
+}
+
 make && (
   if [ ! -d out ]; then
     mkdir out
   fi
   for (( i=0; ; i++ ))
   do  
-    echo gen $i th module
-    ./watsup spec/wasm-2.0/*.watsup --test --test-seed $i
-    ../interpreter/wasm out/"$i".wast
-    if [[ $? = 0 ]]; then
-      echo [Reference interpreter] Success
-    fi
+    # Gen test
+    printf "\n"
+    printf "${ORANGE}Generating $i.wast..${NC}\r"
+    # ./watsup spec/wasm-2.0/*.watsup --test --test-seed $i
+    printf "${CYAN}[$i.wast]           ${NC}\n"
+
+    # Run
+    run "Reference Interpreter" "../interpreter/wasm" "out/${i}.wast"
   done
 
 )
