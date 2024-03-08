fbb9a5484895bbaaeb811d982059ea591995563b
Wonho Shin <wonhoshin@iMac-4.local>
2024-03-08 19:08:07 +0900

    Rename binary

--- spectec/src/backend-test/conform_test.ml
+++ spectec/src/backend-test/conform_test.ml
@@ -9,5 +9,5 @@ let test_engine engine wast =
 
 let conform_test seed =
   let wast = Printf.sprintf "out/%d.wast" seed in
-  test_engine "bin/wasm" wast;
-  test_engine "bin/wasmtime wast" wast;
+  test_engine "../interpreter/wasm" wast;
+  test_engine "wasmtime wast" wast;
