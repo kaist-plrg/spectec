fbf98e2c21c37b8a9e0e9e96d8f31631476094a1
Wonho Shin <wonhoshin@iMac-4.local>
2024-03-12 19:50:29 +0900

    Remove known bugs

--- spectec/src/backend-test/conform_test.ml
+++ spectec/src/backend-test/conform_test.ml
@@ -1,8 +1,19 @@
+let known_msgs = [
+  "cranelift/codegen/src/machinst/lower.rs:766:21";
+]
+
+let is_known_bug msg =
+  Sys.command ("grep " ^ msg ^ " stderr &> /dev/null") = 0
+
 let test_engine engine wast =
   let cmd = engine ^ " " ^ wast in
   Log.debug cmd;
   (* Execute shell command *)
-  let st = Sys.command (cmd ^ "> /dev/null 2> /dev/null") in
+  let st =
+    let res = Sys.command (cmd ^ "> /dev/null 2> stderr") in
+    if res = 0 || List.exists is_known_bug known_msgs then 0
+    else res
+  in
   Log.debug ("Status: " ^ (string_of_int st));
   if st > 0 then
     Log.warn ("`" ^ cmd ^ "` failed")
