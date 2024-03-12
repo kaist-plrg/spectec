abc29298cfe60ab5c4d8c805718797bf7b3dc015
Wonho Shin <wonhoshin@iMac-4.local>
2024-03-12 19:59:58 +0900

    Update conform_test.ml

--- spectec/src/backend-test/conform_test.ml
+++ spectec/src/backend-test/conform_test.ml
@@ -11,15 +11,19 @@ let test_engine engine wast =
   (* Execute shell command *)
   let st =
     let res = Sys.command (cmd ^ "> /dev/null 2> stderr") in
-    if res = 0 || List.exists is_known_bug known_msgs then 0
+    if res = 0 || List.exists is_known_bug known_msgs then
+      0
     else res
   in
   Log.debug ("Status: " ^ (string_of_int st));
   if st > 0 then
-    Log.warn ("`" ^ cmd ^ "` failed")
+    Log.warn ("`" ^ cmd ^ "` failed");
+  st
 
 let conform_test seed =
   let wast = Printf.sprintf "out/%d.wast" seed in
-  test_engine "../interpreter/wasm" wast;
-  test_engine "wasmtime wast" wast;
-  test_engine "wasmer wast" wast;
+  let st_ref = test_engine "../interpreter/wasm" wast in
+  let st_wt = test_engine "wasmtime wast" wast in
+  let st_wr = test_engine "wasmer wast" wast in
+  if st_ref = 0 && st_wt = 0 && st_wr = 0 then
+    Sys.command ("rm " ^ wast) |> ignore
