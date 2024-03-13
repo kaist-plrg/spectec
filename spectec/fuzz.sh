4b05bde27b2a073f4473605eb84d596e3d6b8f85
Wonho Shin <wonhoshin@iMac-4.local>
2024-03-13 16:36:11 +0900

    Minor change

--- spectec/fuzz.sh
+++ spectec/fuzz.sh
@@ -1,3 +1,3 @@
 (cd ../interpreter && make)
 mkdir out
-make && ./watsup spec/wasm-2.0/*.watsup -l --test --test:n $1 --test:log 2
+make && ./watsup spec/wasm-2.0/*.watsup -l --test --test:n $1 --test:log 2 &> log
