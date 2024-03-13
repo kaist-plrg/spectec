808178504f503f5f61df7377e87bac757b3c683b
Wonho Shin <wonhoshin@iMac-4.local>
2024-03-13 16:06:17 +0900

    Minor change

--- spectec/seed.sh
+++ spectec/seed.sh
@@ -1,2 +1,2 @@
 (cd ../interpreter && make)
-make && ./watsup spec/wasm-2.0/*.watsup --test --test:seed $1 --test:log 2
+make && ./watsup spec/wasm-2.0/*.watsup -l --test --test:seed $1 --test:log 2
