c11f53f9d8cd2f862bc82d1b3e6c630d075912aa
f52985 <f52985@users.noreply.github.com>
2025-02-26 08:56:17 +0000

    deploy: 8cda1e6b642ebfd158f2375c9697caa7a9c03cdc

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0 (Draft 2024-07-24)',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.0 (Draft 2025-02-26)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
