7acaa5ee41c6a7daea5ef055c53dcaeb822fe19d
Wonho Shin <gingerbread@Wonhos-MacBook-Pro.local>
2025-03-12 14:13:08 +0900

    Update fix values

--- spectec/src/backend-test/bottom_up.ml
+++ spectec/src/backend-test/bottom_up.ml
@@ -975,9 +975,11 @@ let rec fix_values vt: rule list =
       | CaseE ([[{it = Atom "I31"; _}]], {it = TupE []; _}) ->
         [f "CONST" i32; g "REF.I31"]
       | CaseE ([[{it = Atom "STRUCT"; _}]], {it = TupE []; _}) ->
-        [g "STRUCT.NEW_DEFAULT"]
+        [f "STRUCT.NEW_DEFAULT" vt]
       | CaseE ([[{it = Atom "ARRAY"; _}]], {it = TupE []; _}) ->
-        [f "CONST" i32; g "ARRAY.NEW_DEFAULT"]
+        [f "CONST" i32; f "ARRAY.NEW_DEFAULT" vt]
+      | CaseE ([[{ it = Atom "FUNC"; _ }]], {it = TupE []; _}) ->
+        [f "REF.FUNC" vt]
       | CaseE ([[{it = Atom "_IDX"; _}]; []], _) ->
         let idx = ht |> nth_arg_of_case 0 |> nth_arg_of_case 0 |> exp_to_int in
         let conds = List.filter_map extract_type_sidecond !sideconds in
